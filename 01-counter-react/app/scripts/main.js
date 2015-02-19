ff2545c8c0e86ffe8c2a915008989bd3c1b38d73
Royce Townsend <git@roycetownsend.com>
2015-02-19 16:58:49 +1100

    ReactJs/Counter: Add button, text field and code to increment a number.

--- 01-counter-react/app/scripts/main.js
+++ 01-counter-react/app/scripts/main.js
@@ -1,1 +1,22 @@
-console.log('\'Allo \'Allo!');
+var Counter = React.createClass({
+	getInitialState: function() {
+		return {count: 0};
+	},
+	increment: function(e) {
+		e.preventDefault();
+		this.setState({count: this.state.count + 1});
+	},
+	render: function() {
+		return (
+			React.createElement('div', {className: "eouo"},
+				React.createElement('input', {value: this.state.count}),
+				React.createElement('Button', {onClick: this.increment}, "Count")
+			)
+		);
+	}
+});
+
+React.render(
+	React.createElement(Counter, null),
+	document.getElementById('app')
+);
