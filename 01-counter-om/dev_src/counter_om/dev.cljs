cd08748723b95d06daea15c47c16531e3a240463
Royce Townsend <git@roycetownsend.com>
2015-03-18 15:29:07 +1100

    Om/Counter: Init

---
+++ 01-counter-om/dev_src/counter_om/dev.cljs
@@ -0,0 +1,10 @@
+(ns counter-om.dev
+    (:require
+     [counter-om.core]
+     [figwheel.client :as fw]))
+
+(fw/start {
+  :websocket-url "ws://localhost:3449/figwheel-ws"
+  :on-jsload (fn []
+               ;; (stop-and-start-my app)
+               )})
