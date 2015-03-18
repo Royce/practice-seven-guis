0441ea9488db6f273e0b409b6bc4045c216b8db7
Royce Townsend <git@roycetownsend.com>
2015-03-18 15:31:09 +1100

    Om/Counter: UI + state in root method. Easy.

--- 01-counter-om/src/counter_om/core.cljs
+++ 01-counter-om/src/counter_om/core.cljs
@@ -8,13 +8,23 @@
 
 ;; define your app data so that it doesn't get over-written on reload
 
-(defonce app-state (atom {:text "Hello world!"}))
+(defonce app-state (atom {:count 0}))
+
+(defn inc-count [data]
+  (om/transact! data :count #(inc %)))
 
 (om/root
   (fn [data owner]
     (reify om/IRender
       (render [_]
-        (dom/h1 nil (:text data)))))
+        (dom/div nil
+          (dom/h1 nil "Counter")
+          (dom/input
+            #js {:type "text" :ref "counter"
+                 :value (:count data)})
+          (dom/button
+            #js {:onClick #(inc-count data)}
+            "Count")))))
   app-state
   {:target (. js/document (getElementById "app"))})
 
