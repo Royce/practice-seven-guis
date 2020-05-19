da2958cebf06e6ce96216e659cda1d2181a4ac42
Royce Townsend <git@roycetownsend.com>
2020-05-19 22:38:01 +0800

    [Js/Webpack] Dev server. Hot reloading.

--- js-plainjs-webpack/webpack.config.js
+++ js-plainjs-webpack/webpack.config.js
@@ -1,7 +1,21 @@
 const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
 module.exports = {
+	mode: "development",
 	entry: "./src/index.js",
+	devtool: "inline-source-map",
+	devServer: {
+		contentBase: "./dist",
+		hot: true,
+	},
+	plugins: [
+		new CleanWebpackPlugin({ cleanStaleWebpackAssets: false }),
+		new HtmlWebpackPlugin({
+			title: "Seven GUIs",
+		}),
+	],
 	output: {
 		filename: "main.js",
 		path: path.resolve(__dirname, "dist"),
