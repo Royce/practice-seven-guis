95f6937ee1208b1815b136c7eb09c19ecd5c462f
Royce Townsend <git@roycetownsend.com>
2020-05-19 21:57:13 +0800

    Webpack getting started

---
+++ js-plainjs-webpack/src/index.js
@@ -0,0 +1,11 @@
+import _ from 'Lodash';
+
+function component() {
+	const element = document.createElement("div");
+
+	element.innerHTML = _.join(["Hello", "webpack"], " ");
+
+	return element;
+}
+
+document.body.appendChild(component());
